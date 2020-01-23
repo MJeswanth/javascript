ccad65821bc2ce4b64e5d5f2e9ee72669e91ffab
Jeswanth <mailtojeswanth@uohyd.ac.in>
2020-01-23 23:30:03 +0530

    basics of Java Script

---
+++ basics.js
@@ -0,0 +1,12 @@
+var name = "Jeswanth"; // string
+var age = 26; // number
+var hasHobbies = true; //boolean
+
+
+function userinfo(hasHobbies){
+
+return 'My name is '+name+' of age '+ age+' with hobbies '+hasHobbies;
+
+}
+
+console.log(userinfo('cricket, chess.'));
